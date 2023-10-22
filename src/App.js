b6bbe187972d78ff56ad1d55ea62e2018b577903
rakshith-rk20 <rakshithvk143@gmail.com>
2023-10-22 22:32:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Detailsfetch from "./components/Detailsfetch";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App bg-dark container-fluid">
+      <Detailsfetch />
+    </div>
+  );
+}
+
+export default App;
